4a577901e032d975344359d3eae6c5e5b214c5e1
KazutoMartin <75247334+KazutoMartin@users.noreply.github.com>
2022-03-20 16:05:20 +0330

    added notebook adn rf

--- genre_detection.py
+++ genre_detection.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import numpy as np
+import sklearn
+from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
 
@@ -25,7 +29,9 @@ from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("Data/genres_v2.csv")
 
-df = df.drop(columns=['type', 'id', 'uri', 'track_href','analysis_url', 'song_name','Unnamed: 0','title', 'duration_ms','time_signature'])
+# print(df.head())
+df = df.drop(columns=['type', 'id', 'uri', 'track_href','analysis_url', 'song_name','Unnamed: 0','title', 'duration_ms','time_signature',
+                      'mode', 'key'])
 # df = df.drop(['type'])
 
 
@@ -35,7 +41,7 @@ df['genre'] = df['genre'].replace(['Dark Trap', 'Rap', 'Pop', 'Hiphop', 'Trap Me
 # loudness can have some negative data
 df["loudness"] = df["loudness"] + df["loudness"].min()
 
-df["key"] = (df["key"] / df["key"].max())
+# df["key"] = (df["key"] / df["key"].max())
 df["tempo"] = (df["tempo"] / df["tempo"].max())
 df["loudness"] = (df["loudness"] / df["loudness"].max())
 
@@ -46,18 +52,55 @@ df["loudness"] = (df["loudness"] / df["loudness"].max())
 df = df.sample(frac=1)
 # df_random.to_csv('now.csv')
 
-knn = KNeighborsClassifier(n_neighbors=61)
 
 X = df.drop(columns=['genre'])
 y = df['genre']
 
+# from sklearn import linear_model
+
+# regr = linear_model.LinearRegression()
+
+
+
+# alg = KNeighborsClassifier(n_neighbors=20)
+# alg = DecisionTreeClassifier()
+alg = RandomForestClassifier()
+# print(X.head())
+# print(y.head())
+alg.fit(X, y)
+test = {
+        "danceability": 0.508,
+        "energy": 0.792,
+        # "key": 0,
+        "loudness": -7.311,
+        "mode": 0,
+        "speechiness": 0.0297,
+        "acousticness": 0.229,
+        "instrumentalness": 0.124,
+        "liveness": 0.145,
+        "valence": 0.601,
+        "tempo": 147.067,
+        }
+# print(alg.predict([[
+#     test['danceability'],
+#     test['energy'],
+#     # test['key'] / df["key"].max(), 
+#     (test['loudness'] + df["loudness"].min()) / df["loudness"].max(),
+#     test['mode'],
+#     test['speechiness'],
+#     test['acousticness'],
+#     test['instrumentalness'],
+#     test['liveness'],
+#     test['valence'],
+#     test['tempo'] / df["tempo"].max()
+# ]]))
 
 # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
 
 
 # cross_validate
-cv_scores = cross_val_score(knn, X, y, cv=10)
+cv_scores = cross_val_score(alg, X, y, cv=10)
 
 cv_scores_mean = np.mean(cv_scores)
 print(cv_scores , "\n\n""mean =" ,"{:.2f}".format(cv_scores_mean))
